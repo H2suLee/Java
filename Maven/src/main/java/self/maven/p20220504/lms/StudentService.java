d75c75975cee35c1c633e5bba2fce31a517c2bc2
H2suLee <103907953+H2suLee@users.noreply.github.com>
2022-05-12 10:21:33 +0900

    Update StudentService.java

--- Maven/src/main/java/self/maven/p20220504/lms/StudentService.java
+++ Maven/src/main/java/self/maven/p20220504/lms/StudentService.java
@@ -13,14 +13,14 @@ import java.util.List;
 //Delete 삭제
 
 public class StudentService {
-
 	// 필드
+	Scanner sc = new Scanner(System.in);
 	private StudentVO student;
 	private List<StudentVO> students = new ArrayList<>();
 	// List는 인터페이스라서 ArrayList, LinkedList, QueueList로만 구현 가능함
 
 	// 메소드
-	private void dataSet() {
+	public void dataSet() {
 		student = new StudentVO("lee", "이영흠", "컴퓨터공학", "서울시", "010-1111-1111", Date.valueOf("1996-02-27")); // 0행
 		students.add(student);
 
@@ -36,31 +36,86 @@ public class StudentService {
 
 	// 1. 전체 학생 조회 - forEach문
 	public void selectList() {
-		dataSet();
+//		dataSet(); // 이 초기화는 바깥에서 해줄 것
 		for (StudentVO vo : students) {
 			System.out.println("-----------------------------");
 			vo.toString(); // toString() 오버라이딩 해 주어야
 		}
+
+//		for (int i = 0; i < students.size(); i++) {
+//			System.out.println("-----------------------------");
+//			StudentVO std = students.get(i);
+//			std.toString();
+//		}
 	}
 
 	// 2. 한 명의 학생 조회 (학번을 입력받아)
 	public void selectStudent(String id) {
-		dataSet();
 		for (StudentVO vo : students) {
 			if (vo.getId().equals(id)) {
 				System.out.println("검색한 학생 ▶");
-				vo.toString(); 
+				vo.toString();
 			}
 		}
 	}
-	
+
 	// 3. 학생 등록
 	public void insertStudent(StudentVO vo) {
-		dataSet();
 		students.add(vo);
-		for(StudentVO std : students) {
+		for (StudentVO std : students) {
 			std.toString();
 		}
 	}
 
+	// 4. 학생 수정
+	public void updateStudent(String inputID) {
+		System.out.println("수정할 내역을 선택해 주세요");
+		System.out.println("1. id  |  2. 이름  |  3. 전공  |  4. 주소  |  5. 전화번호  |  6. 생년월일  ");
+		int num = sc.nextInt();
+		switch (num) {
+		case 1:
+			System.out.println("수정할 id를 입력해 주세요");
+			String updateID = sc.next();
+			for (StudentVO vo : students) {
+				if (vo.getId().equals(inputID)) {
+					vo.setId(updateID);
+					System.out.println("====== 아래와 같이 수정되었습니다.=====");
+					vo.toString();
+				}
+			}
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+
+		}
+
+	}
+
+	// 5. 학생 삭제
+	public void deleteStudent(String deleteID) {
+		for (StudentVO vo : students) {
+			if (vo.getId().equals(deleteID)) {
+				System.out.println("삭제할 학생 ▶");
+				vo.toString();
+				System.out.println("정말 삭제하시겠습니까?");
+				System.out.println("1. 네(Y)  |  2. 아니오(N)");
+				String yesNo = sc.next();
+				if (yesNo.equals("N")) {
+					return;
+				} else {
+					students.remove(vo);
+					System.out.println("삭제를 완료하였습니다.");
+					break;
+				}
+			}
+		}
+	}
 }
